5fe60a97887fd6297415bcc328d4236e1582441e
abdul92306 <abdul.rahman6340@gmail.com>
2017-10-16 00:06:29 -0700

    first commit

---
+++ resources/views/master/super.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+   <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<script
+  src="https://code.jquery.com/jquery-3.2.1.js"
+  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+  crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+</head>
+<body>
+
+@include('partial.header')
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/partial/header.blade.php
@@ -0,0 +1,26 @@
+<nav class = "col-lg-2 navbar navbar-default h_color" role = "navigation">
+   	   <div class="row content">
+   <div class = "navbar-header h_class col-sm-2">
+      <button type = "button" class = "navbar-toggle" 
+         data-toggle = "collapse" data-target = "#example-navbar-collapse">
+         <span class = "sr-only">Toggle navigation</span>
+         <span class = "icon-bar"></span>
+         <span class = "icon-bar"></span>
+         <span class = "icon-bar"></span>
+      </button>
+		
+      <a class = "navbar-brand" href = "#">Jhon Wick</a>
+   </div>
+
+   <div class="navbar-text col-md-6 container-fluid collapse navbar-collapse "
+    id = "example-navbar-collapse" >
+	    <form action="/action_page.php">
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="Search" name="search">
+      <div class="input-group-btn">
+        <button class="btn btn-info icon" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+      </div>
+    </div>
+  </form>
+   </div>
+</nav>
